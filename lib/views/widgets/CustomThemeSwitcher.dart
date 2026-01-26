95e5cb2c2301e10074eb586e44358ecfdbefe2b1
Adam Sanchez <adam.sanchez37320@gmail.com>
2026-01-26 13:49:29 +0100

    Fix: Update the Splash Screen

--- lib/views/widgets/CustomThemeSwitcher.dart
+++ lib/views/widgets/CustomThemeSwitcher.dart
@@ -139,7 +139,7 @@ class _CustomThemeSwitcherState extends State<CustomThemeSwitcher>
               // Inverted icons: dark -> sun, light -> moon.
               isDark ? Icons.light_mode : Icons.dark_mode,
               size: totalHeight * 0.04,
-              color: isDark ? Color(0xFFEFEFEF) : Color(0xFF242424),
+              color: isDark ? Color(0xFFFFFFFF) : Color(0xFF242424),
             ),
           ),
         ),
